1d0281934c70a08e51429668e0a88656bec0ad08
Luke Oliff <luke@lukeoliff.com>
2026-02-09 23:40:19 +0000

    feat(auth): add JWT session auth with page nonce

--- starter/urls.py
+++ starter/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.serve_index, name='index'),
+    path('api/session', views.get_session, name='session'),
     path('api/metadata', views.metadata, name='metadata'),
 ]
